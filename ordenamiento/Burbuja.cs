4a2f6c5c09340aae1d1b6764a213b33047207f1e
carlos8520 <cavalencia10@gmail.com>
2019-03-21 09:56:51 -0600

    ordenamiento burbuja

---
+++ ordenamiento/Burbuja.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collection.Generic;
+using System.text;
+
+namespace ordenamiento
+{
+    e ordenamiento;
+    public class Burbuja
+    {
+        public void ordenar(int [] arr) {
+            throw new NotImplementedException("Crear los casos de prueba");
+        }
+    }
+}
