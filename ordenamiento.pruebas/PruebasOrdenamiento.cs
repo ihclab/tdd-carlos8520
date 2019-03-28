5fd3f2ff9cd6de5744108032105bde8cc2a0a85b
20136357 CARLOS ABRAHAM VALENCIA TORRES <20136357@TELEMATICA.UCOL.MX>
2019-03-28 07:58:04 -0600

    burbuja y métodos de prueba terminados

--- ordenamiento.pruebas/PruebasOrdenamiento.cs
+++ ordenamiento.pruebas/PruebasOrdenamiento.cs
@@ -15,9 +15,37 @@ namespace ordenamiento.pruebas
         [TestMethod]
         public void Ordenados()
         {
-            int[] entrada = new int[]{5,4,3,2,1};
+            int[] entrada = new int[]{1,2,3,4,5};
             ordenar.Ordenar(entrada);
             CollectionAssert.AreEqual(salidaEsperada, entrada);
+        }
+
+        [TestMethod]
+        public void Desordenados() {
+            int[] entrada = new int[]{1, 3, 2, 4, 5};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada1, entrada);
+        }
+
+        [TestMethod]
+        public void UltimoPrincipio() {
+            int[] entrada = new int[]{5, 1, 2, 3, 4};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada1, entrada);
+        }
+
+        [TestMethod]
+        public void PrimeroFinal() {
+            int[] entrada = new int[]{2, 3, 4, 5, 1};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada1, entrada);
+        }
+
+        [TestMethod]
+        public void Invetido() {
+            int[] entrada = new int[]{5, 4, 3, 2, 1};
+            ordenar.Ordenar(entrada);
+            CollectionAssert.AreEqual(salidaEsperada1, entrada);
         }
     }
 }
--- ordenamiento/Burbuja.cs
+++ ordenamiento/Burbuja.cs
@@ -8,12 +8,15 @@ namespace Ordenamiento
     {
         public void Ordenar(int [] arr) {
             throw new NotImplementedException("Crear los casos de prueba");
+            int aux = 0;
 
-            for(int i = 0; i < arr.Length - 1; i++) {
-                if(arr[i] > arr[i + 1]) {
-                    int aux = arr[i];
-                    arr[i] = arr[i+1];
-                    arr[i + 1] = aux;
+            for(int i = 0; i < arr.Length; i++) {
+                for(int j = 0; j < arr.Length-1; j++) {
+                    if(arr[j] > arr[j + 1]) {
+                    aux = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = aux;
+                    }
                 }
             }
         }
